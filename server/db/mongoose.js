9495020039021ce0913e232c2210b0cbc675c592
BlackZero172 <Supermeega@outlook.com>
2022-01-20 23:21:28 +0200

    Added env Vars

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const dbURL = "mongodb+srv://ali:123123123@bankapi.jqsj6.mongodb.net/bankAPI?retryWrites=true&w=majority";
-
+require("dotenv").config();
+const dbURL = process.env.MONGO_URL;
 mongoose.connect(dbURL, { useNewUrlParser: true }, (err, client) => {
 	if (err) console.log(err);
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 require("./db/mongoose");
 const path = require("path");
