9537080b9fdc6cd26a45380cf77d125137a3f64b
BlackZero172 <Supermeega@outlook.com>
2022-01-20 19:54:31 +0200

    Served React App Through Express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 require("./db/mongoose");
+const path = require("path");
+
+const { getReact } = require("./api/controllers/utils/utils");
 const cors = require("cors");
 const User = require("./models/user");
 const port = process.env.PORT || 5555;
@@ -8,6 +11,7 @@ const router = require("./api/routes/user.routes");
 server.use(cors());
 server.use(express.json());
 server.use("/api", router);
+server.use(express.static(getReact()));
 server.listen(port, () => {
 	console.log(`Server is up and listening on ${port}`);
 });
