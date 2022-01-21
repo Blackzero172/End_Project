c485e01d89ad0218828d7059f1e0021b5107e690
BlackZero172 <Supermeega@outlook.com>
2022-01-21 14:58:44 +0200

    Added Custom Input and Button Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import CustomInput from "./components/CustomInput/CustomInput.components";
+import CustomButton from "./components/CustomButton/CustomButton.components";
 function App() {
-	return <div>Testing</div>;
+	return (
+		<div>
+			<div className="input-container">
+				<CustomInput placeHolder="Enter username..." />
+				<CustomButton text="Sort By:Highest to Lowest" />
+				<CustomButton text="Sort By:Highest to Lowest" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
